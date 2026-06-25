84084a187b63b21b3542ea913307fb9062983541
Bhymi M Manoj <byme9876@gmail.com>
2026-06-26 00:39:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///factory.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Database Model
+class FactoryData(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    temperature = db.Column(db.Integer)
+
+    machine_status = db.Column(db.String(10))
+
+    efficiency = db.Column(db.Integer)
+
+# Home Route
+@app.route('/')
+def home():
+
+    temperature = random.randint(25, 45)
+
+    machine_status = random.choice(["ON", "OFF"])
+
+    efficiency = random.randint(70, 100)
+
+    # Save data to database
+    new_data = FactoryData(
+        temperature=temperature,
+        machine_status=machine_status,
+        efficiency=efficiency
+    )
+
+    db.session.add(new_data)
+
+    db.session.commit()
+
+    return render_template(
+        'index.html',
+        temperature=temperature,
+        machine_status=machine_status,
+        efficiency=efficiency
+    )
+@app.route('/history')
+def history():
+
+    all_data = FactoryData.query.all()
+
+    return render_template(
+        'history.html',
+        records=all_data
+    )
+@app.route('/api/data')
+def api_data():
+
+    latest = FactoryData.query.order_by(
+        FactoryData.id.desc()
+    ).first()
+
+    return {
+
+        "temperature": latest.temperature,
+
+        "machine_status": latest.machine_status,
+
+        "efficiency": latest.efficiency
+
+    }
+if __name__ == '__main__':
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
+    from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///factory.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Database Model
+class FactoryData(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    temperature = db.Column(db.Integer)
+
+    machine_status = db.Column(db.String(10))
+
+    efficiency = db.Column(db.Integer)
+
+# Home Route
+@app.route('/')
+def home():
+
+    temperature = random.randint(25, 45)
+
+    machine_status = random.choice(["ON", "OFF"])
+
+    efficiency = random.randint(70, 100)
+
+    # Save data to database
+    new_data = FactoryData(
+        temperature=temperature,
+        machine_status=machine_status,
+        efficiency=efficiency
+    )
+
+    db.session.add(new_data)
+
+    db.session.commit()
+
+    return render_template(
+        'index.html',
+        temperature=temperature,
+        machine_status=machine_status,
+        efficiency=efficiency
+    )
+
+if __name__ == '__main__':
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
